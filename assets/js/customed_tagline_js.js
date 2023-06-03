7fdb7dcb21888d4b4ee87aa822540dcdad915968
Panagiotis Kiriakopoulos <panagkpetr@gmail.com>
2023-06-03 20:48:02 +0300

    index-projects-about

---
+++ assets/js/customed_tagline_js.js
@@ -0,0 +1,29 @@
+var input = './taglines';
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+fetch(input)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('File not found');
+    }
+    return response.text();
+  })
+  .then(data => {
+    const lines = data.split('\n');
+    lines.forEach(line => {
+      // Do something with each line
+    //   console.log(line);
+    });
+    var i = getRandomInt(0, lines.length);
+    var selectedline = lines[i];
+    console.log(selectedline);
+    document.getElementById("taglineh2").innerHTML = selectedline;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
